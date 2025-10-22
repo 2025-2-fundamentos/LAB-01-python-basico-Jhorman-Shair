3ffa7b624c9ff47861d52adc8529ee12d6c93ee9
Jhorman-Shair <jhramirezhe@unal.edu.co>
2025-10-22 10:18:12 -0500

    primer envio

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,7 +5,6 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
-
 def pregunta_01():
     """
     Retorne la suma de la segunda columna.
@@ -14,3 +13,4 @@ def pregunta_01():
     214
 
     """
+    return sum([int(line.split("\t")[1]) for line in open("files/input/data.csv")])
